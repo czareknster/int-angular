85ea77878d696edbfbae95c12c7e6fb159845fcc
CezarMac <cezarmac@CezarMacs-MacBook-Pro.local>
2022-11-21 23:26:35 +0100

    test

--- .history/libs/users/src/lib/views/user-view/user-view.component_20221121231748.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'interview-angular-user-view',
-  templateUrl: './user-view.component.html',
-  styleUrls: ['./user-view.component.scss'],
-})
-export class UserViewComponent {}
